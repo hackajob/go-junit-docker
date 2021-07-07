e88cf95c76128826a34abe28a831eb3b76fe7cd6
Vlad Iacob <vlad.iacob@hackajob.co>
2021-07-07 12:28:08 +0300

    Fix: Run test cases with the new junit image

--- test-and-report.sh
+++ test-and-report.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /go/src/app
-go test -mod=vendor -v $(go list ./... | grep -v /test) | go-junit-report > report.xml
+go test -mod=vendor -v $(go list -mod=vendor ./... | grep -v /test) | go-junit-report > report.xml
