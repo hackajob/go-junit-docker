0aec294214c2404462107453c70c0f3be7f15a33
vladiacob <vlad.iacob@hackajob.co>
2024-10-10 10:16:34 +0300

    Update test-and-report.sh

--- test-and-report.sh
+++ test-and-report.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /go/src/app
-go test -mod=vendor -buildvcs=false -v $(go list -mod=vendor ./... | grep -v /test) | go-junit-report > report.xml
+go test -mod=vendor -buildvcs=false -v $(go list -buildvcs=false -mod=vendor ./... | grep -v /test) | go-junit-report > report.xml
